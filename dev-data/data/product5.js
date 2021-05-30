2e2da7b6bfb1be47c3e12b36cb1ea99000f59dc3
MagentaSpruce <magentaspruce@protonmail.com>
2021-05-29 20:10:27 -0400

    Finished constructing and formatting JSON Docs. Everything is finished front end besides media queries.

---
+++ dev-data/data/product5.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+const product5 = {
+  id: 5,
+  name: 'Soothing Relief',
+  storedAs: 'Loose Herb',
+  cost: 17,
+  medicinalProperties: 'Anti-inflammatory, tissue repair',
+  amount: '4 Oz',
+  avgRating: 4.9,
+  numReviews: 19,
+  regPrice: 17,
+  shortDescription:
+    'Specifically formulated for the perineum. Utilizes Yarrow to help sooth inflammation and encourage tissue healing. Perfect for baths.',
+  longDescription:
+    'Nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nVoluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur!',
+};
