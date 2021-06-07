d4d99b4a6b1266aa33a572eb1a43de13a9f8fa8c
MagentaSpruce <magentaspruce@protonmail.com>
2021-06-07 13:16:22 -0400

    Created an config.env file for enviornmental varbailes to be stored in. Created a script for starting a production enviornment with different parameters than the development enviornment.

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const productRouter = require('./routes/productRoutes');
 const herbRouter = require('./routes/herbRoutes');
 
 const app = express();
-
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log(
