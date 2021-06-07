1fcee935c23628d48a6a7b22aa10f4f95317a415
MagentaSpruce <magentaspruce@protonmail.com>
2021-06-07 13:47:02 -0400

    Created, configured, implemented and tested eslint to get it working for this project.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`App running on port ${port}`);
 });
