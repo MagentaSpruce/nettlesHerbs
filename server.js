db071bc02377ec69419672bf75b46264578af719
MagentaSpruce <magentaspruce@protonmail.com>
2021-06-07 13:48:21 -0400

    Wass error on last submission detailing my setting up eslinter for this project. This is a resubmission.

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const app = require('./app');
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`App running on port ${port}`);
+  console.log(`App running on port ${port}.`);
 });
