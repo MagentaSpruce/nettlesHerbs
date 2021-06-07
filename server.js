60a62ba2d3dd86fba05001ce112d6d0fb0f61c29
MagentaSpruce <magentaspruce@protonmail.com>
2021-06-07 19:00:09 -0400

    Connected the Express application with the remote db on atlas. Created a product schema and model and used them to successfully create new product documents for testing purposes.

--- server.js
+++ server.js
@@ -1,8 +1,52 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
-
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  // eslint-disable-next-line no-console
+  .then(() => console.log('DB connection is successful! 💋'));
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A product must have a name.'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A product must have a price.']
+  }
+});
+
+const Product = mongoose.model('Product', productSchema);
+const testProduct2 = new Product({
+  name: 'The Herb2',
+  price: 9
+});
+
+testProduct2
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('error 😒', err);
+  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
