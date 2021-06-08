219a530cdb279300fae818b78c7e36dedca30975
MagentaSpruce <magentaspruce@protonmail.com>
2021-06-08 11:31:32 -0400

    Added the getAllProducts() handler function.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,25 +1,37 @@
 const Product = require('./../models/productModel.js');
 
-exports.getProduct = (req, res) => {
-  //   console.log(req.params);
-  // const id = req.params.id * 1;
-  // const product = products.find(el => el.id === id);
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     products: product
-  //   }
-  // });
+exports.getProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        product
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
 };
 
 exports.getAllProducts = async (req, res) => {
-  const products = await Product.find();
+  try {
+    const products = await Product.find();
 
-  res.status(200).json({
-    status: 'success',
-    results: products.length,
-    data: { products: products }
-  });
+    res.status(200).json({
+      status: 'success',
+      results: products.length,
+      data: { products: products }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
 };
 
 exports.createProduct = async (req, res) => {
@@ -40,13 +52,24 @@ exports.createProduct = async (req, res) => {
   }
 };
 
-exports.updateProduct = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      product: 'We will update you baby!'
-    }
-  });
+exports.updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        product: 'We will update you baby!'
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
 };
 
 exports.deleteProduct = (req, res) => {
