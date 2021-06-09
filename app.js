7fd8e9f2e09e01a2678db0c6f62219f7c1ee289e
MagentaSpruce <magentaspruce@protonmail.com>
2021-06-09 18:30:59 -0400

    Created a global error handling middleware function.

--- app.js
+++ app.js
@@ -29,9 +29,19 @@ app.use('/api/v1/products', productRouter);
 app.use('/api/v1/herbs', herbRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
