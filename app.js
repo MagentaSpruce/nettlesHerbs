c1658afdfb8002c95b8521b8523e677386305537
MagentaSpruce <magentaspruce@protonmail.com>
2021-06-12 08:57:10 -0400

    Set up sending the JWT as a cookie instead of a string for better security.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,20 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() +
+        Number(process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000)
+    ),
+    httpOnly: true
+  };
+  // So we can test sending cookies in postman w/out using encrypted: (secure = true)
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove the password from the output
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
