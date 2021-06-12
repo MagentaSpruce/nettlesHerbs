b1fce8cc41bcf3794e740f3e15736425ef1cf03a
MagentaSpruce <magentaspruce@protonmail.com>
2021-06-12 09:26:41 -0400

    Implemented rate limiting and set security HTTP headers to increase security.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-const rateLimit = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,14 +9,32 @@ const productRouter = require('./routes/productRoutes.js');
 const userRouter = require('./routes/userRoutes.js');
 
 const app = express();
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
+
+// GLOBAL MIDDLEWARE
+// Set security HTTP headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// Body parser - reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Testing MW
 app.use((req, res, next) => {
   console.log(
     'This is baby cakes completely useless middleware coming at you live scribe, do you dig it? 🕵️‍♀️'
@@ -23,6 +42,7 @@ app.use((req, res, next) => {
   next();
 });
 
+// Sets time of request
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
